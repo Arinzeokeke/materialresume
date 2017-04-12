9b22ed2a51f8b06d3b91369362219e193118ce27
Okeke Arinze <trent9002@gmail.com>
2017-04-12 21:08:25 +0100

    Switched to js for removing models

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function(){
+	$(".remove_fields").on("click", function(e){
+		e.preventDefault()
+		$(this).prev("input[type=hidden]").val('1');
+		$(this).closest('fieldset').hide()
+	})
+})

bc6382d5856d6b64601ad7a60c66530d3c3d2ae3
Okeke Arinze <trent9002@gmail.com>
2017-04-22 20:53:15 +0100

    scratch that. actual Style fix

--- app/assets/javascripts/cv_form/form.js
+++ app/assets/javascripts/cv_form/form.js
@@ -6,3 +6,22 @@
 //= require cv_form/jquery.bootstrap.js
 //= require cv_form/material-bootstrap-wizard.js
 //= require cv_form/jquery.validate.min.js
+$(function(){
+	$(".remove_fields").on("click", function(e){
+		console.log('jk');
+		e.preventDefault();
+		$(this).prev("input[type=hidden]").val('1');
+		$(this).closest('fieldset').hide();
+	})
+
+	$(".add_fields").on('click', function(e){
+		e.preventDefault()
+		console.log("clicked")
+		var time = new Date().getTime()
+		var regex = new RegExp($(this).data('id'), 'g')
+		//console.log($(this).before.html())
+		$(this).before($(this).data('field').replace(regex, time))
+
+		e.preventDefault()
+	})
+})

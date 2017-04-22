e8f15377800266c546453fb7c1beaf7c1c789836
Okeke Arinze <trent9002@gmail.com>
2017-04-22 23:05:23 +0100

    clear posts.js

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,21 +1,2 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(function(){
-	$(".remove_fields").on("click", function(e){
-		console.log('jk');
-		e.preventDefault();
-		$(this).prev("input[type=hidden]").val('1');
-		$(this).closest('fieldset').hide();
-	})
-
-	$(".add_fields").on('click', function(e){
-		e.preventDefault()
-		console.log("clicked")
-		var time = new Date().getTime()
-		var regex = new RegExp($(this).data('id'), 'g')
-		//console.log($(this).before.html())
-		$(this).before($(this).data('field').replace(regex, time))
-
-		e.preventDefault()
-	})
-})

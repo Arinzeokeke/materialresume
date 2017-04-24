35c4d5c2efdb5e49e9d23ecd5a057d1e3fcd58b9
Okeke Arinze <trent9002@gmail.com>
2017-04-24 21:48:35 +0100

    Devise initialized. User devise model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   get 'auth/login'
 
   get 'auth/logout'
--- db/migrate/20170424202328_devise_create_users.rb
+++ db/migrate/20170424202328_devise_create_users.rb
@@ -30,6 +30,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :username
 
       t.timestamps null: false
     end

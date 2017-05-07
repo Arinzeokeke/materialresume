c11b78b2a52d96631df7dab0297fefb5a5ba9487
Okeke Arinze <trent9002@gmail.com>
2017-05-07 17:53:38 +0100

    refactoring. using clases insted of id for modal target. modal functional now

--- app/assets/javascripts/cv_form/form.js
+++ app/assets/javascripts/cv_form/form.js
@@ -7,42 +7,8 @@
 //= require cv_form/material-bootstrap-wizard.js
 //= require cv_form/jquery.validate.min.js
 $(document).on("turbolinks:load", function(){
-	$("#signup-btn").click( function(){
-	console.log('modal yo');
-	$("#signin-modal").modal('hide');
-	$("#signup-modal").modal();
-		$("#signup-error").html('');
-	$("#signin-error").html('');
-});
 
 
-	$("#signin-btn").on('click', function(){
-	console.log('modal yo 2');
-	$("#signup-modal").modal('hide');
-	$("#signin-modal").modal();
-		$("#signup-error").html('');
-	$("#signin-error").html('');
-
-});
-
-
-$("#signin2").on('click', function(){
-	console.log('modal yo 2');
-	$("#signup-modal").modal('hide');
-	$("#signin-modal").modal();
-		$("#signup-error").html('');
-	$("#signin-error").html('');
-
-});
-
-
-$("#signup2").click( function(){
-	console.log('modal yo');
-	$("#signin-modal").modal('hide');
-	$("#signup-modal").modal();
-		$("#signup-error").html('');
-	$("#signin-error").html('');
-});
 	$(".remove_fields").on("click", function(e){
 		console.log('jk');
 		e.preventDefault();
@@ -55,65 +21,89 @@ $("#signup2").click( function(){
 		console.log("clicked");
 		var time = new Date().getTime();
 		var regex = new RegExp($(this).data('id'), 'g');
-		//console.log($(this).before.html())
 		$(this).before($(this).data('field').replace(regex, time));
-
 	});
 
 
-
-
 	var show_ajax_message = (msg) => {
-  		$("#signin-error").html("<div id='flash-#{type}'>" + msg + "</div>");
-  		//$("#flash-#{type}").delay(5000).slideUp('slow');
-  	}
-
-	$(document).ajaxComplete((event, request) => {
-  		msg = request.getResponseHeader("X-Message");
-  		type = request.getResponseHeader("X-Message-Type");
-  		//show_ajax_message(msg, type); //use whatever popup, notification or whatever plugin you want
-  	});
-	
-
-
-  $("#sign_in_user").on("ajax:success", (e, data, status, xhr) => {
-    //$("#new_article").append(xhr.responseText);
-
-    //console.log(data);
-    console.log(status);
-    console.log(xhr);
-    
-    $("#signin-error").html("Logged In!");
+		$("#signin-error").html("<div id='flash-#{type}'>" + msg + "</div>");
+//$("#flash-#{type}").delay(5000).slideUp('slow');
 }
-  ).on ("ajax:error", (e, xhr, status, error) => {
-    //$("#new_article").append("<p>ERROR</p>");
-    //console.log(data);
-    console.log(status);
-    console.log(xhr);
-    var errors = JSON.parse(xhr.responseText).message;
-    var mes = "";
-    mes += '<p>' + errors + '</p>';
-    mes +=  '';
-    $("#signin-error").html(errors);
-    console.log(xhr.responseText);
+
+$(document).ajaxComplete((event, request) => {
+	msg = request.getResponseHeader("X-Message");
+	type = request.getResponseHeader("X-Message-Type");
+//show_ajax_message(msg, type); //use whatever popup, notification or whatever plugin you want
 });
 
-   $("#sign_up_user").on("ajax:success", (e, data, status, xhr) => {
-    //$("#new_article").append(xhr.responseText);
-    console.log(xhr.responseText);
-    $("#signup-error").html("Signed Up!");
+
+
+$("#sign_in_user").on("ajax:success", (e, data, status, xhr) => {
+
+
+	$("#signin-error").html("Logged In!");
 }
-  ).on ("ajax:error", (e, xhr, status, error) => {
-    //$("#new_article").append("<p>ERROR</p>");
-    console.log(xhr.responseText);
-    var errors = JSON.parse(xhr.responseText);
-    console.log(errors);
-    var mes = "<div class = 'row'>";
-    for (i in errors){
-    	mes += '<p>'  + errors[i] + '</p>';
-    }
-    mes +=  '</div>';
-    $("#signup-error").html(mes);
+).on ("ajax:error", (e, xhr, status, error) => {
+//$("#new_article").append("<p>ERROR</p>");
+//console.log(data);
+console.log(status);
+console.log(xhr);
+var errors = JSON.parse(xhr.responseText).message;
+var mes = "";
+mes += '<p>' + errors + '</p>';
+mes +=  '';
+$("#signin-error").html(errors);
+console.log(xhr.responseText);
 });
+
+$("#sign_up_user").on("ajax:success", (e, data, status, xhr) => {
+//$("#new_article").append(xhr.responseText);
+console.log(xhr.responseText);
+$("#signup-error").html("Signed Up!");
+}
+).on ("ajax:error", (e, xhr, status, error) => {
+//$("#new_article").append("<p>ERROR</p>");
+console.log(xhr.responseText);
+var errors = JSON.parse(xhr.responseText);
+console.log(errors);
+var mes = "<div class = 'row'>";
+for (i in errors){
+	mes += '<p>'  + errors[i] + '</p>';
+}
+mes +=  '</div>';
+$("#signup-error").html(mes);
+});
+
+$(".signup-btn").click( function(){
+	console.log('modal yo');
+	$("#signin-modal").modal('hide');
+	$("#signup-modal").modal();
+	$("#signup-error").html('');
+	$("#signin-error").html('');
+	$("#reg-modal").modal('hide');
+});
+
+
+$(".signin-btn").on('click', function(){
+	console.log('modal yo 2');
+	$("#signup-modal").modal('hide');
+	$("#signin-modal").modal();
+	$("#signup-error").html('');
+	$("#signin-error").html('');
+	$("#reg-modal").modal('hide');
+
+});
+
+
+
+$("#reg-warn").on('click', function(){
+	console.log('modal yo 2');
+	$("#signin-modal").modal('hide');
+	$("#signup-modal").modal('hide');
+	$("#reg-modal").modal();
+
+});
+
+
 
 });
--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -15,155 +15,82 @@ $(document).on("turbolinks:load", () => {
 
 
 	var show_ajax_message = (msg) => {
-  		$("#signin-error").html("<div id='flash-#{type}'>" + msg + "</div>");
-  		//$("#flash-#{type}").delay(5000).slideUp('slow');
-  	}
-
-	$(document).ajaxComplete((event, request) => {
-  		msg = request.getResponseHeader("X-Message");
-  		type = request.getResponseHeader("X-Message-Type");
-  		//show_ajax_message(msg, type); //use whatever popup, notification or whatever plugin you want
-  	});
-	
-
-
-  $("#sign_in_user").on("ajax:success", (e, data, status, xhr) => {
-    //$("#new_article").append(xhr.responseText);
-
-    //console.log(data);
-    console.log(status);
-    console.log(xhr);
-    
-    $("#signin-error").html("Logged In!");
+		$("#signin-error").html("<div id='flash-#{type}'>" + msg + "</div>");
+//$("#flash-#{type}").delay(5000).slideUp('slow');
 }
-  ).on ("ajax:error", (e, xhr, status, error) => {
-    //$("#new_article").append("<p>ERROR</p>");
-    //console.log(data);
-    console.log(status);
-    console.log(xhr);
-    var errors = JSON.parse(xhr.responseText).message;
-    var mes = "";
-    mes += '<p>' + errors + '</p>';
-    mes +=  '';
-    $("#signin-error").html(errors);
-    console.log(xhr.responseText);
+
+$(document).ajaxComplete((event, request) => {
+	msg = request.getResponseHeader("X-Message");
+	type = request.getResponseHeader("X-Message-Type");
+//show_ajax_message(msg, type); //use whatever popup, notification or whatever plugin you want
 });
 
-   $("#sign_up_user").on("ajax:success", (e, data, status, xhr) => {
-    //$("#new_article").append(xhr.responseText);
-    console.log(xhr.responseText);
-    $("#signup-error").html("Signed Up!");
+
+
+$("#sign_in_user").on("ajax:success", (e, data, status, xhr) => {
+
+
+$("#signin-error").html("Logged In!");
 }
-  ).on ("ajax:error", (e, xhr, status, error) => {
-    //$("#new_article").append("<p>ERROR</p>");
-    console.log(xhr.responseText);
-    var errors = JSON.parse(xhr.responseText);
-    console.log(errors);
-    var mes = "<div class = 'row'>";
-    for (i in errors){
-    	mes += '<p>'  + errors[i] + '</p>';
-    }
-    mes +=  '</div>';
-    $("#signup-error").html(mes);
+).on ("ajax:error", (e, xhr, status, error) => {
+//$("#new_article").append("<p>ERROR</p>");
+//console.log(data);
+console.log(status);
+console.log(xhr);
+var errors = JSON.parse(xhr.responseText).message;
+var mes = "";
+mes += '<p>' + errors + '</p>';
+mes +=  '';
+$("#signin-error").html(errors);
+console.log(xhr.responseText);
 });
 
-$("#signup-btn").click( function(){
+$("#sign_up_user").on("ajax:success", (e, data, status, xhr) => {
+//$("#new_article").append(xhr.responseText);
+console.log(xhr.responseText);
+$("#signup-error").html("Signed Up!");
+}
+).on ("ajax:error", (e, xhr, status, error) => {
+//$("#new_article").append("<p>ERROR</p>");
+console.log(xhr.responseText);
+var errors = JSON.parse(xhr.responseText);
+console.log(errors);
+var mes = "<div class = 'row'>";
+for (i in errors){
+	mes += '<p>'  + errors[i] + '</p>';
+}
+mes +=  '</div>';
+$("#signup-error").html(mes);
+});
+
+$(".signup-btn").click( function(){
 	console.log('modal yo');
 	$("#signin-modal").modal('hide');
 	$("#signup-modal").modal();
 	$("#signup-error").html('');
 	$("#signin-error").html('');
+	$("#reg-modal").modal('hide');
 });
 
 
-$("#signin-btn").on('click', function(){
+$(".signin-btn").on('click', function(){
 	console.log('modal yo 2');
 	$("#signup-modal").modal('hide');
 	$("#signin-modal").modal();
 	$("#signup-error").html('');
 	$("#signin-error").html('');
+	$("#reg-modal").modal('hide');
 
 });
 
 
-$("#signin2").on('click', function(){
-	console.log('modal yo 2');
-	$("#signup-modal").modal('hide');
-	$("#signin-modal").modal();
-
-});
 
 $("#reg-warn").on('click', function(){
 	console.log('modal yo 2');
+	$("#signin-modal").modal('hide');
+	$("#signup-modal").modal('hide');
 	$("#reg-modal").modal();
 
-});
-
-
-$("#signup2").click( function(){
-	console.log('modal yo');
-	$("#signin-modal").modal('hide');
-	$("#signup-modal").modal();
-});
-
-
-
-$('.ajax-signin-btn').on('click', () => {
-	var email = $('#login-email').val();
-	var password = $('#login-password').val();
-	var remember = $('#login-remember').val();
-
-	$.ajax({
-		url: '/users/sign_in.json',
-		type: 'post',
-		contentType: 'application/json',
-		dataType: 'json',
-				headers: {
-	 	'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-		},
-
-		data: JSON.stringify({
-			'user': {
-				'email': email,
-				'password': password,
-				'remember_me': remember
-			}
-		}),
-
-		success: (data) => {
-			console.log(data);
-		}
-	});
-});
-
-
- $('.ajax-signup-btn').on('click', () => {
-	var email = $('#signup-email').val();
-	var username = $('#signup-user').val();
-	var password = $('#signup-password').val();
-	var password_confirm = $('#signup-password-confirm').val();
-
-	$.ajax({
-		url: '/users/sign_up.json',
-		type: 'post',
-		contentType: 'application/json',
-		dataType: 'json',
-		headers: {
-	 	'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
-		},
-		data: JSON.stringify({
-			'user': {
-				'email': email,
-				'username': username,
-				'password': password,
-				'password_confirmation': password_confirm
-			}
-		}),
-
-		success: (data) => {
-			console.log(data);
-		}
-	});
 });
 	
 });
